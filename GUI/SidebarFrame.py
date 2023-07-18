e1a6744daee3c7a786f18c967e015ce41bf824da
sthd <sch@campus.technion.ac.il>
2023-07-18 10:20:52 +0300

    added sidebr frame

---
+++ GUI/SidebarFrame.py
@@ -0,0 +1,54 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+
+from DataToCSV.stringsForModifiers import NatureOfRights_mappings
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class SidebarFrame(customtkinter.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent, width=140, corner_radius=0)
+
+        self.grid_rowconfigure(4, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self, text="Sidebar GUI",
+                                                 font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+
+        self.sidebar_button_1 = customtkinter.CTkButton(self, command=self.sidebar_button_event)
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+
+        self.sidebar_button_2 = customtkinter.CTkButton(self, command=self.sidebar_button_event)
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+
+        self.sidebar_button_3 = customtkinter.CTkButton(self, command=self.sidebar_button_event)
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+
+        self.sidebar_button_4 = customtkinter.CTkButton(self, command=self.sidebar_button_event)
+        self.sidebar_button_4.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_button_4.configure(text="4th bu")
+
+        self.appearance_mode_label = customtkinter.CTkLabel(self, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionmenu = customtkinter.CTkOptionMenu(self, values=["Light", "Dark", "System"],
+                                                                      command=self.change_appearance_mode_event)
+        self.appearance_mode_optionmenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+
+        self.scaling_label = customtkinter.CTkLabel(self, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionmenu = customtkinter.CTkOptionMenu(self, values=["80%", "90%", "100%", "110%", "120%"],
+                                                              command=self.change_scaling_event)
+        self.scaling_optionmenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+
+
+    def sidebar_button_event(self):
+        print("sidebar_button click")
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
